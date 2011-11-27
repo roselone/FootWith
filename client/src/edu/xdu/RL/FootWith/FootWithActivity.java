3367b5ce2618c9f64ac4ae62cc39942ee884caac
roselone <roselonelh@gmail.com>
2011-11-27 15:25:28 +0800

    create client

---
+++ client/src/edu/xdu/RL/FootWith/FootWithActivity.java
@@ -0,0 +1,13 @@
+package edu.xdu.RL.FootWith;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FootWithActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
