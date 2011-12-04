34ddac149fb09a9909b38b3b456a4586c4fef933
roselone <roselonelh@gmail.com>
2011-12-04 16:58:11 +0800

    main page design

--- client/gen/edu/xdu/RL/FootWith/R.java
+++ client/gen/edu/xdu/RL/FootWith/R.java
@@ -8,24 +8,38 @@
 package edu.xdu.RL.FootWith;
 
 public final class R {
+    public static final class anim {
+        public static final int zoom_enter=0x7f040000;
+        public static final int zoom_exit=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int background_login=0x7f020000;
-        public static final int background_login_div0=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int background_login_button=0x7f020001;
+        public static final int background_login_div0=0x7f020002;
+        public static final int background_main=0x7f020003;
+        public static final int background_tab=0x7f020004;
+        public static final int home=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int me=0x7f020007;
+        public static final int news=0x7f020008;
     }
     public static final class id {
-        public static final int button_login=0x7f060006;
-        public static final int button_register=0x7f060007;
-        public static final int layout_logo=0x7f060008;
-        public static final int login_div=0x7f060001;
-        public static final int login_password_input=0x7f060004;
-        public static final int login_user_input=0x7f060002;
-        public static final int logo_div=0x7f060000;
-        public static final int logo_text=0x7f060009;
-        public static final int password_edit=0x7f060005;
-        public static final int username_edit=0x7f060003;
+        public static final int button_login=0x7f070006;
+        public static final int button_register=0x7f070007;
+        public static final int login_div=0x7f070001;
+        public static final int login_password_input=0x7f070004;
+        public static final int login_user_input=0x7f070002;
+        public static final int logo_div=0x7f070000;
+        public static final int logo_text=0x7f070008;
+        public static final int main_logo=0x7f070009;
+        public static final int password_edit=0x7f070005;
+        public static final int tab1=0x7f07000b;
+        public static final int tab2=0x7f07000c;
+        public static final int tab3=0x7f07000d;
+        public static final int tabhost=0x7f07000a;
+        public static final int username_edit=0x7f070003;
     }
     public static final class layout {
         public static final int login=0x7f030000;
@@ -33,18 +47,21 @@ public final class R {
         public static final int main=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int login_button=0x7f040005;
-        public static final int login_label_password=0x7f040003;
-        public static final int login_label_username=0x7f040002;
-        public static final int login_username_hint=0x7f040004;
-        public static final int logo_text=0x7f040007;
-        public static final int register_button=0x7f040006;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+        public static final int login_button=0x7f050005;
+        public static final int login_label_password=0x7f050003;
+        public static final int login_label_username=0x7f050002;
+        public static final int login_username_hint=0x7f050004;
+        public static final int logo_text=0x7f050007;
+        public static final int register_button=0x7f050006;
+        public static final int tab1_text=0x7f050008;
+        public static final int tab2_text=0x7f050009;
+        public static final int tab3_text=0x7f05000a;
     }
     public static final class style {
-        public static final int graySmallText=0x7f050001;
-        public static final int logoText=0x7f050002;
-        public static final int whiteSmallText=0x7f050000;
+        public static final int graySmallText=0x7f060001;
+        public static final int logoText=0x7f060002;
+        public static final int whiteSmallText=0x7f060000;
     }
 }
--- client/src/edu/xdu/RL/FootWith/FootWithActivity.java
+++ client/src/edu/xdu/RL/FootWith/FootWithActivity.java
@@ -1,8 +1,13 @@
 package edu.xdu.RL.FootWith;
 
+import edu.xdu.RL.FootWith.helper.intentHelper;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
 public class FootWithActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,6 +15,32 @@ public class FootWithActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.login);
+        setContentView(R.layout.main);
+        
+        TabHost mainTabHost=(TabHost)findViewById(R.id.tabhost);
+        mainTabHost.setup();
+        
+        TabHost.TabSpec spec=mainTabHost.newTabSpec("tab1");  
+        spec.setContent(R.id.tab1);  
+        spec.setIndicator("",getResources().getDrawable(R.drawable.home));
+        mainTabHost.addTab(spec);  
+          
+        spec=mainTabHost.newTabSpec("tab2");  
+        spec.setContent(R.id.tab2);  
+        spec.setIndicator("",getResources().getDrawable(R.drawable.news));  
+        mainTabHost.addTab(spec);  
+          
+        spec=mainTabHost.newTabSpec("tab3");  
+        spec.setContent(R.id.tab3);  
+        spec.setIndicator("",getResources().getDrawable(R.drawable.me));  
+        mainTabHost.addTab(spec); 
+        
+        TabWidget tabWidget=mainTabHost.getTabWidget();
+        
+        for (int i=0;i<tabWidget.getChildCount();i++){
+        	View view = tabWidget.getChildTabViewAt(i);
+        }
+        mainTabHost.setCurrentTab(0);  
+        
     }
 }
