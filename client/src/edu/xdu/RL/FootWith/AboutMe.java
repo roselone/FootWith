b0497156e72f16387b138eee6c5b4c7327829d1f
roselone <roselonelh@gmail.com>
2011-12-09 10:34:52 +0800

    add create new Trip Page

--- client/src/edu/xdu/RL/FootWith/AboutMe.java
+++ client/src/edu/xdu/RL/FootWith/AboutMe.java
@@ -6,10 +6,17 @@ import java.util.HashMap;
 import edu.xdu.RL.FootWith.helper.Myself;
 import edu.xdu.RL.FootWith.helper.MyselfAdapter;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class AboutMe extends Activity {
@@ -42,6 +49,30 @@ public class AboutMe extends Activity {
 				String type=(String)map.get("itemType");
 				
 				if (type.equals("newTrip")){
+               		LayoutInflater inflater=(LayoutInflater)AboutMe.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            		final RelativeLayout layout=(RelativeLayout)inflater.inflate(R.layout.aboutme_newtrip, null);
+            		AlertDialog.Builder builder=new AlertDialog.Builder(AboutMe.this);
+            		builder.setTitle("编辑您的旅程").setView(layout);
+            		builder.setPositiveButton("发布", new DialogInterface.OnClickListener() {
+
+						@Override
+						public void onClick(DialogInterface arg0, int arg1) {
+							// TODO Auto-generated method stub
+							
+						}
+            		});
+            		builder.setNegativeButton("算了", new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface arg0, int arg1) {
+							// TODO Auto-generated method stub
+							
+						}
+					});
+            		builder.show();
+				}
+				
+				if (type.equals("trip")){
 					
 				}
 				
