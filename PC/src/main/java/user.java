0487c7da50edc9ca9c3e08ea50e2b25a3c81771d
roselone <roselone@me.com>
2013-03-12 16:56:02 +0800

    create project

---
+++ PC/src/main/java/user.java
@@ -0,0 +1,15 @@
+import java.lang.String;
+import java.lang.System;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: roselone
+ * Date: 3/12/13
+ * Time: 4:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class user {
+    public static void main(String args[]){
+        System.out.print("Hello World!");
+    }
+}
