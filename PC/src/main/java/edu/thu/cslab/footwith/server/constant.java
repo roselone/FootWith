0a81ffd90a3a3dff038628109ba8b9cf1def0ca9
roselone <roselone@me.com>
2013-03-16 00:05:41 +0800

    modify sqlscript,insert site data

---
+++ PC/src/main/java/edu/thu/cslab/footwith/server/constant.java
@@ -0,0 +1,12 @@
+package edu.thu.cslab.footwith.server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: roselone
+ * Date: 3/15/13
+ * Time: 10:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class constant {
+    public final String PROJECT_PATH = "/Users/roselone/git/FootWith/";
+}
