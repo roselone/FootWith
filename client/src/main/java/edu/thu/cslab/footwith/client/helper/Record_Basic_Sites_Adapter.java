ddc89e8e5a975fec5dd9d6abb52f88003dd5c026
bxl1989 <bxl1989@gmail.com>
2013-05-07 19:58:36 +0800

    icons resource

---
+++ client/src/main/java/edu/thu/cslab/footwith/client/helper/Record_Basic_Sites_Adapter.java
@@ -0,0 +1,11 @@
+package edu.thu.cslab.footwith.client.helper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bxl
+ * Date: 5/2/13
+ * Time: 10:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Record_Basic_Sites_Adapter {
+}
